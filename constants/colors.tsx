91e8838b06da0d0d1fdd15f85909b0a026b28359
Adriana <adrianasotojimenez3@gmail.com>
2025-09-26 01:42:54 -0400

    feat(colors): agregar paleta de colores para crear una pagina

--- constants/colors.tsx
+++ constants/colors.tsx
@@ -4,6 +4,17 @@ export default {
   danger: "#B70F0A",     // icono
   accent: "#B70F0A",     // texto
   white: "#FFFFFF",      // texto claro
-  black: "#000000", 
-  Grey: "#555"  // texto oscuro
+  black: "#000000",
+  Grey: "#555", // texto oscuro
+
+  pageColors: [
+    "#FFFFFF",
+    "#98E07C",
+    "#C7F36B",
+    "#8AE0FF",
+    "#C692FF",
+    "#FFA08A",
+    "#FF7B6E",
+    "#FFE34D",
+  ],
 };
